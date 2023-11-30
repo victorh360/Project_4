1a62b67bbee1aee1e6becd450efca54840eac567
imennajar <imen668669@gmail.com>
2023-11-29 23:55:55 -0500

    Update LSTM model and adjust related files

--- price.sql
+++ price.sql
@@ -5,10 +5,6 @@ CREATE TABLE prices (
 	Price_beef_kilo DECIMAL,
 	Price_rice_kilo DECIMAL,
 	Price_coffee_kilo DECIMAL,
-	Inflation_rate DECIMAL,
-	Price_rice_infl DECIMAL,
-	Price_beef_infl DECIMAL,
-	Price_coffee_infl DECIMAL
 	PRIMARY KEY(Year,Month)
 );
 
